8a37e77c481dc6dc4e4779dd69bc3224b046ad2a
yamasakih <yamasaki.phone@gmail.com>
2024-02-12 17:49:11 +0900

    :art: Trial 002

--- main.py
+++ main.py
@@ -23,14 +23,13 @@ P: list[list[float]]
 C: list[list[float]]
 
 
-def debug_p(to_int: bool = True) -> None:
-    if to_int:
+def debug_p(to_int: float = -1) -> None:
+    if to_int == -1:
         for p in P:
-            debug(f"{AHC30} {' '.join('0' if pi < 1 else str(int(pi)) for pi in p)}")
+            debug(f"{AHC30} {' '.join(format(pi, '.2f') for pi in p)}")
     else:
         for p in P:
-            # debug(f"{AHC30} {' '.join(str(pi) for pi in p)}")
-            debug(f"{AHC30} {' '.join(format(pi, '.2f') for pi in p)}")
+            debug(f"{AHC30} {' '.join('0' if pi < to_int else '1' for pi in p)}")
 
 
 def play_random() -> None:
@@ -39,20 +38,15 @@ def play_random() -> None:
     C = [[0] * N for _ in range(N)]
     # ランダムな場所、ランダムな大きさの矩形で何回か占う
     times = 10000
+    bottom, up = 2, 4
     while times > 0:
         # 左上、右下の座標をランダムに求める
-        left_up_x = randint(0, N - 1)
-        right_bottom_x = randint(0, N - 1)
-        if left_up_x == right_bottom_x:
-            continue
-        if left_up_x > right_bottom_x:
-            left_up_x, right_bottom_x = right_bottom_x, left_up_x
-        left_up_y = randint(0, N - 1)
-        right_bottom_y = randint(0, N - 1)
-        if left_up_y == right_bottom_y:
-            continue
-        if left_up_y > right_bottom_y:
-            left_up_y, right_bottom_y = right_bottom_y, left_up_y
+        width = randint(bottom, up)
+        height = randint(bottom, up)
+        left_up_x = randint(0, N - 1 - width)
+        left_up_y = randint(0, N - 1 - height)
+        right_bottom_x = left_up_x + width
+        right_bottom_y = left_up_y + height
         # 占う
         times -= 1
         fields = []
@@ -69,9 +63,9 @@ def play_random() -> None:
             for y in range(left_up_y, right_bottom_y + 1):
                 P[x][y] = (P[x][y] * C[x][y] + oil_value) / (C[x][y] + 1)
                 C[x][y] += 1
-    debug_p(to_int=False)
+    debug_p(to_int=-1)
     debug(f"{AHC30} ---------------------------")
-    debug_p(to_int=True)
+    debug_p(to_int=0.3)
     # コストを確認する
     print("c")
 
